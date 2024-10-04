0927181c03a01ba7a0cf93ad3092dce77d5fd42f
Luis Felipe Cremonez <105985428+felipecremonez@users.noreply.github.com>
2024-10-04 15:23:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Seleciona os elementos do DOM
-const spam = document.getElementById('window');
+const spam = document.getElementById('mainButton');
 const inputArquivo = document.getElementById('inputArquivo');
 
 // Adiciona um evento de clique ao spam
@@ -16,9 +16,26 @@ inputArquivo.addEventListener('change', (event) => {
 });
 
 function openWindow() {
-    let window = document.getElementById('window');
-    window.style.display = 'flex';
-
     let input = document.getElementById('inputArquivo');
     input.click();
 }
+
+
+  // Função para validar se o arquivo é MP3
+function validarArquivoMP3(arquivo) {
+    const extensaoValida = arquivo.name.endsWith('.mp3');
+    const tipoValido = arquivo.type === 'audio/mpeg';
+    return extensaoValida && tipoValido;
+}
+
+// Adiciona um evento de mudança ao input de arquivo
+inputArquivo.addEventListener('change', (event) => {
+    const arquivoSelecionado = event.target.files[0];
+    if (arquivoSelecionado) {
+        if (validarArquivoMP3(arquivoSelecionado)) {
+            alert(`Você selecionou um arquivo MP3 válido: ${arquivoSelecionado.name}`);
+        } else {
+            alert('Por favor, selecione um arquivo MP3 válido.');
+        }
+    }
+})
